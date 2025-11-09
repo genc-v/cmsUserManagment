a19cfc13dbf218543ca36fcd2a81dfff533be911
Genc Vllahiu <genc.v@gjirafa.com>
2025-11-10 00:05:20 +0100

    testing pre push

--- cmsUserManagment.API/Program.cs
+++ cmsUserManagment.API/Program.cs
@@ -17,7 +17,7 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString))
-)
+);
 
 var redisOptionsConfiguration = builder.Configuration["Redis:Connection"];
 
